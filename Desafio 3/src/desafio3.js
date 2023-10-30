dc340910e848fe3ae2038f2ec3cc066f47080b76
Codnova <codnova@gmail.com>
2023-10-30 00:25:49 -0300

    Added 'limit' query support for the /productos endpoint

--- Desafio 3/src/desafio3.js
+++ Desafio 3/src/desafio3.js
@@ -13,7 +13,7 @@ class ProductManager {
       return JSON.parse(data)
     } catch (error) {
       if (error.code === 'ENOENT') {
-        // Archivo no existe
+        //Archivo no existe
         return []
       } 
       throw error
@@ -48,7 +48,6 @@ class ProductManager {
       let productFound = products.find(product => product.id === id)
       if(!productFound) {
         console.log('No se encontró el producto con ID: ', id)
-        //throw new Error ('Producto no encontrado en getProductById')
       }
       return productFound; //Retornamos el producto encontrado
 
@@ -68,7 +67,7 @@ class ProductManager {
       } else {
         console.log(`El producto ${products[index].title} ha sido borrado`)
         products.splice(index, 1)
-        await fs.writeFile(this.path, JSON.stringify(products, null, 5)) // Guardamos los cambios en el archivo
+        await fs.writeFile(this.path, JSON.stringify(products, null, 5)) //Guardamos los cambios en el archivo
       }
     } catch (error) {
       console.error('Error borrando el producto con ID: ', id)
@@ -78,7 +77,7 @@ class ProductManager {
 
   async updateProduct(id, object) {
     try {
-      const allowedProperties = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'] // Propiedades que están habilitadas para su modificación
+      const allowedProperties = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'] //Propiedades que están habilitadas para su modificación
       let products = await this.getProducts()
       let index = products.findIndex(product => product.id === id);
       if (index === -1) {
@@ -91,7 +90,7 @@ class ProductManager {
               products[index][key] = object[key];
           }
       }
-        await fs.writeFile(this.path, JSON.stringify(products, null, 5)) // Guardamos los cambios en el archivo
+        await fs.writeFile(this.path, JSON.stringify(products, null, 5)) //Guardamos los cambios en el archivo
       }
     } catch(error){
         console.error('No se actualizó el producto con ID: ', id)
@@ -110,25 +109,29 @@ async function main(){
   const PORT=3000
   
   const app = express()
+
+  app.use(express.urlencoded({extended:true}))
   
-  app.get('/', (req, res)=>{ // Para responder a los request en / (landing page)
+  app.get('/', (req, res)=>{ //Para responder a los request en / (landing page)
     res.setHeader('Content-Type', 'text/html')
     res.send("Página de Bienvenida")
     
   })
   
-  app.get('/productos', async (req, res)=>{ // Devuelve la lista completa de productos
-    res.setHeader('Content-Type', 'application/json') // Seteamos el header
+  app.get('/productos', async (req, res)=>{ //Devuelve la lista completa de productos
+    res.setHeader('Content-Type', 'application/json') //Seteamos el header
     let productos = await productManager.getProducts()
+    let limit = parseInt(req.query.limit) //Obtenemos el query del limite de productos
+    productos = productos.slice(0, limit) //Modificamos el array para limitar los resultados
     res.send({productos})
     console.log(productos)
   })
 
-  app.get('/productos/:id', async (req, res)=>{ // Devuelve un producto por su ID
-    res.setHeader('Content-Type', 'application/json') // Seteamos el header
+  app.get('/productos/:id', async (req, res)=>{ //Devuelve un producto por su ID
+    res.setHeader('Content-Type', 'application/json') //Seteamos el header
     let productos = await productManager.getProducts()
     let id= req.params.id //Este param viene en formato string
-    id = parseInt(id) // Transformamos el ID de string a numero
+    id = parseInt(id) //Transformamos el ID de string a numero
     if(isNaN(id)){
       return res.send('el ID ingresado no es un numero')
     }
